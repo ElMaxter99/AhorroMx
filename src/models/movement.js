ea66eb26dd1b5bbf90590ea46d7149021adf3eef
ElMaxter99 <themaxter99@gmail.com>
2025-03-22 23:36:52 +0100

    refactor movementModel

--- src/models/movement.js
+++ src/models/movement.js
@@ -1,39 +1,32 @@
 const mongoose = require('mongoose');
 const { TYPE } = require('../enums/movement');
 
-const movementSchema = new mongoose.Schema({
-  user: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'User',
-    required: true,
-  },
-  type: {
-    type: String,
-    enum:  Object.values(TYPE),
-    required: true,
-  },
-  amount: {
-    type: Number,
-    required: true,
-  },
-  description: {
-    type: String,
-    trim: true,
-  },
-  category: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'Category',
-    required: false,
-  },
-  relatedGroupExpense: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'GroupExpense',
-    required: false,
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now,
-  },
-});
+const movementSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+    },
+    type: {
+      type: String,
+      enum: Object.values(TYPE),
+      required: true,
+    },
+    amount: {
+      type: Number,
+      required: true,
+    },
+    description: {
+      type: String,
+      trim: true,
+    },
+    category: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Category',
+      required: false,
+    },
+  }, { timestamps: true}
+);
 
 module.exports = mongoose.model('Movement', movementSchema);
