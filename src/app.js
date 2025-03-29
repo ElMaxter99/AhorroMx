c02eeefce4c1eb8479f592c7cd8682aff8b31502
Alvaro Maxter <themaxter99@gmail.com>
2025-03-29 17:13:57 +0100

    change .env to config

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 
+const config = require('../config');
 const routes = require('./routes');
 const errorHandler = require('./middlewares/errorHandler');
 
@@ -10,7 +11,7 @@ const app = express();
 app.use(express.json());
 app.use(errorHandler);
 
-mongoose.connect(process.env.MONGO_URI, {
+mongoose.connect(config.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 }).then(() => console.log('MongoDB connected'))
